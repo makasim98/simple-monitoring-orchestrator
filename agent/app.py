fb82d76d934da58afb0960f358b885063e00eb1a
makasim98 <maxluch98@gmail.com>
2025-08-12 13:38:32 -0400

    Basic structure

---
+++ agent/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask("ovserver-agent")
+
+monitored_endpoints = {}
+
+@app.route('/')
+def dashboard():
+    return "<h1>Orchestrator Dashboard!!!</h1>" # return HTML
+
+@app.route('/add')
+def add_endpoint():
+    return "<h1>ADDING</h1>" # return HTML
+
+@app.route('/remove')
+def remove_endpoint():
+    return "<h1>REMOVING</h1>" # return HTML
+
+app.run(host="0.0.0.0", port=5000, debug=True)
