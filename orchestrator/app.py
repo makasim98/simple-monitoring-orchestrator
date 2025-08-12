8e1a1c56932abf403f188214ecf4f0c0002d20bb
Joseph <busterglobe12@gmail.com>
2025-08-12 12:06:38 -0600

    message

--- orchestrator/app.py
+++ orchestrator/app.py
@@ -6,7 +6,22 @@ monitored_endpoints = {}
 
 @app.route('/')
 def dashboard():
-    return "<h1>Orchestrator Dashboard!!!</h1>" # return HTML
+    return '''
+    <h1>Orchestrator Dashboard!!!</h1>
+        <p>Manage your monitored endpoints here.</p>
+            <div style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 16px; width: 400px; margin: 40px auto;">
+                <div style="background: #ffffff; border: 1px solid #000000; height: 100px; display: flex; align-items: center; justify-content: center;">Box 1</div>
+                <div style="background: #ffffff; border: 1px solid #000000; height: 100px; display: flex; align-items: center; justify-content: center;">Box 2</div>
+                <div style="background: #ffffff; border: 1px solid #000000; height: 100px; display: flex; align-items: center; justify-content: center;">Box 3</div>
+                <div style="background: #ffffff; border: 1px solid #000000; height: 100px; display: flex; align-items: center; justify-content: center;">Box 4</div>
+                <div style="background: #ffffff; border: 1px solid #000000; height: 100px; display: flex; align-items: center; justify-content: center;">Box 5</div>
+                <div style="background: #ffffff; border: 1px solid #000000; height: 100px; display: flex; align-items: center; justify-content: center;">Box 6</div>
+                <div style="background: #ffffff; border: 1px solid #000000; height: 100px; display: flex; align-items: center; justify-content: center;">Box 7</div>
+                <div style="background: #ffffff; border: 1px solid #000000; height: 100px; display: flex; align-items: center; justify-content: center;">Box 8</div>
+                <div style="background: #ffffff; border: 1px solid #000000; height: 100px; display: flex; align-items: center; justify-content: center;">Box 9</div>
+            </div>
+            '''
+
 
 @app.route('/add')
 def add_endpoint():
